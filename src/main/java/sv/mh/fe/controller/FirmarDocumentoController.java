994830769d6fc5d679a2711378de79e1630625d3
rogelioGuerrero <rogelioGuerrero@users.noreply.github.com>
2026-02-21 14:53:19 -0600

    fix: Agregar return statement faltante en FirmarDocumentoController
    - Corregir error de compilación por return statement faltante en caso dteObject null

--- src/main/java/sv/mh/fe/controller/FirmarDocumentoController.java
+++ src/main/java/sv/mh/fe/controller/FirmarDocumentoController.java
@@ -64,6 +64,8 @@ public class FirmarDocumentoController extends Controller {
 						response.put("jws", firma);
 						
 						return ResponseEntity.ok(response.toMap());
+					} else {
+						return ResponseEntity.ok(mensaje.error(Errores.COD_811_CONVERTIR_STRING_A_JSON, "DTE object is null"));
 					}										
 				} catch (JsonProcessingException e) {
 					logger.info(errores.COD_810_CONVERTIR_JSON_A_STRING, e.getMessage());
